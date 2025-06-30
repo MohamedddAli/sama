e102aeb65915906c78ffc4abb87d13fa6910388c
mohamedddAli <moamedamr584@gmail.com>
2025-07-01 02:42:33 +0300

    edited app.js

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,30 @@ import cors from "cors";
 // Import routes
 import productRouter from "./routes/product.js";
 import categoryRouter from "./routes/category.js";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
+// Load environment variables
+dotenv.config();
 const app = express();
+
+// Set up port and MongoDB URI
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("✅ MongoDB connected");
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("❌ MongoDB connection failed:", err.message);
+  });
+
+//-------------------------------------------------------------------------------------------------
 
 // Middleware
 app.use(express.json());
