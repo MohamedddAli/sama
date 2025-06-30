8aae8bf3dc861a48e31b4b4304468ce3b857892d
mohamedddAli <moamedamr584@gmail.com>
2025-07-01 02:14:33 +0300

    updated cors

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL, // only allow your frontend to access the backend
+    origin: "https://sama-int.vercel.app", // only allow your frontend to access the backend
     credentials: true, // if using cookies or auth headers
   })
 );
